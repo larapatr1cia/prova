e9fd970b56e46758e6a706c84504f3208e1589ba
Lara Patricia <larapatriciadev@gmail.com>
2022-04-24 17:08:44 -0300

    Submit

--- prova.js
+++ prova.js
@@ -1,11 +1,11 @@
 function getMovieInfosOMDB(title){
-    const url = `https://www.omdbapi.com/?t=${title}&apikey=790af7bc`  
+    const url = `https://www.omdbapi.com/?t=${title}&apikey=790af7bc`  /*é uma rotina, normalmente escrita com a linguagem T-SQL, que executa uma ação (como um cálculo complexo) e retorna o resultado dessa ação como um valor */
        
-    fetch(url)
-    .then(response => response.json())
+    fetch(url) /*fornece uma maneira fácil e lógica para buscar recursos de forma assíncrona através da rede*/
+    .then(response => response.json()) /*O json()método da Responseinterface recebe um Responsefluxo e o lê até a conclusão. Ele retorna uma promessa que resolve com o resultado da análise do corpo do texto como JSON.*/
     .then(data => {
 
-        if(data.Response == 'False'){
+        if(data.Response == 'False'){ /* Se data.Response for falso, não vai exibir as informações a seguir*/
             descriptionBodyNotFound.textContent = "Filme não encontrado! Tente novamente.";
             descriptionBodyNotFound.style.marginBottom = '30px'
             movieTitle.textContent = "Titulo do filme"
@@ -18,7 +18,7 @@ function getMovieInfosOMDB(title){
             movieDirector.textContent = "Nome do Diretor"
             moviePoster.style.backgroundImage = `url(filmeNaoEncontrado.png)`
 
-        } else {
+        } else { /*Se não, vai exibir as informações do filme como titulo, genero, diretor etc */
             descriptionBodyNotFound.textContent = "";
             
             movieTitle.textContent = data.Title
@@ -36,10 +36,10 @@ function getMovieInfosOMDB(title){
      })  
 }
 
-var form = document.getElementById('formSearch');
+var form = document.getElementById('formSearch'); /*Declara as variaveis do formulario */
 var title = document.getElementById('title');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) { /*Registra uma única espera de evento em um único alvo */
     
     getMovieInfosOMDB(title.value)
     // impede o envio do form
